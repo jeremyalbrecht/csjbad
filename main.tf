5f75813adfee40a359d4d9f715acaa9ff85aa2b5
Jérémy Albrecht <me@jalbrecht.fr>
2024-07-11 17:26:37 +0200

    feat: add IaC

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "augny-badminton"
+  location = "West Europe"
+}
+
+resource "azurerm_static_site" "augny-badminton" {
+  name                = "augny-badminton-fr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
